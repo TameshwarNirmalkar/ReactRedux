8518d15b40ec09f455cfaa559f55e79002ee29e3
Tameshwar Nirmalkar <tameshwar.nirmalkar@scrumstart.net>
2019-05-28 13:21:02 +0530

    code refactor

--- src/components/CustomContent.js
+++ src/components/CustomContent.js
@@ -9,90 +9,89 @@ import OpenImage from '../components/OpenImage';
 function mapStateToProps(state) {
 
 	return {
-		customContentReducerObj : state.customContentReducer
+		customContentReducerObj: state.customContentReducer
 	};
 }
 
 function mapDispatchToProps(dispatch) {
 
 	return {
-		actions : {
-			setJsonData : function(){
+		actions: {
+			setJsonData: function () {
 				return dispatch(setJsonData());
 			},
-			openImageModal : function(targetImageId,staticJsonData){
-                return dispatch(openImageModal(targetImageId,staticJsonData));
-            },
-            closeImageModal : function(){
-                return dispatch(closeImageModal());
-            },
-            deleteImage : function(elementId,staticDeleteData){
-            	return dispatch(deleteImage(elementId,staticDeleteData));
-            }
+			openImageModal: function (targetImageId, staticJsonData) {
+				return dispatch(openImageModal(targetImageId, staticJsonData));
+			},
+			closeImageModal: function () {
+				return dispatch(closeImageModal());
+			},
+			deleteImage: function (elementId, staticDeleteData) {
+				return dispatch(deleteImage(elementId, staticDeleteData));
+			}
 		}
 	};
 }
 
 
-class CustomContent extends Component
-{
+class CustomContent extends Component {
 
-	constructor(props){
+	constructor(props) {
 		super(props);
 		this.handleOpenClick = this.handleOpenClick.bind(this);
 		this.deleteImage = this.deleteImage.bind(this);
 		this.printImage = this.printImage.bind(this);
 	}
 
-	componentDidMount(){
+	componentDidMount() {
 		this.props.actions.setJsonData();
 	}
 
 	componentDidUpdate(prevProps, prevState) {
 
-	var classSelector = document.getElementsByClassName('imgClass');
-		for(var i =0 ; i< classSelector.length ; i++){
-			classSelector[i].addEventListener("mouseenter",this.mouseEnterCallback);
-			classSelector[i].addEventListener("mouseleave",this.mouseLeaveCallback);
+		var classSelector = document.getElementsByClassName('imgClass');
+		for (var i = 0; i < classSelector.length; i++) {
+			classSelector[i].addEventListener("mouseenter", this.mouseEnterCallback);
+			classSelector[i].addEventListener("mouseleave", this.mouseLeaveCallback);
 		}
 	}
 
-	mouseEnterCallback(){
+	mouseEnterCallback() {
 		var getElementId = this.id;
 		var element = document.getElementById(getElementId);
 		element.className += " " + "hover";
 	}
 
-	mouseLeaveCallback(){
+	mouseLeaveCallback() {
 		var getElementId = this.id;
 		var element = document.getElementById(getElementId);
 		element.className = " imgClass";
 	}
 
-	handleOpenClick(e){
+	handleOpenClick(e) {
 		var targetImageId = e.target.id;
 		var staticJsonData = this.props.customContentReducerObj.contentData;
-		this.props.actions.openImageModal(targetImageId,staticJsonData);
+		this.props.actions.openImageModal(targetImageId, staticJsonData);
 	}
 
-	deleteImage(e){
+	deleteImage(e) {
 		var deleteImageIdWithString = e.target.id;
 		var deleteImageCombinedId = deleteImageIdWithString.split("_");
 		var staticDeleteData = this.props.customContentReducerObj.deleteImage;
-		this.props.actions.deleteImage(deleteImageCombinedId[1],staticDeleteData);
+		this.props.actions.deleteImage(deleteImageCombinedId[1], staticDeleteData);
 	}
 
-	printImage(e){
+	printImage(e) {
 		var printImageWithString = e.target.id;
 		var printImageCombinedId = printImageWithString.split("_");
 		var printImageId = printImageCombinedId[1];
-		var staticData= this.props.customContentReducerObj.contentData;
-		
-		for(var key in staticData){
-			if(staticData[key]['id'] == printImageId){
+		var staticData = this.props.customContentReducerObj.contentData;
+
+		for (var key in staticData) {
+			if (staticData[key]['id'] == printImageId) {
 				var popup;
 
-				popup = window.open( staticData[key]['imageLocation'] );
+				popup = window.open(staticData[key]['imageLocation']);
 				popup.onbeforeunload = this.closePrint;
 				popup.onafterprint = this.closePrint;
 				popup.focus();
@@ -101,49 +100,49 @@ class CustomContent extends Component
 		}
 	}
 
-	closePrint () {
-		if ( popup ) {
+	closePrint() {
+		if (popup) {
 			popup.close();
 		}
 	}
 
-	render(){
+	render() {
 		var imageContentHtml = [];
 		var imageContentFields = this.props.customContentReducerObj.contentData;
-		var i=1;
-		
-		for(var key in imageContentFields){
+		var i = 1;
+
+		for (var key in imageContentFields) {
 			imageContentHtml.push(
-				<div className = {"col-md-3 col-sm-6 col-xs-12" + this.props.customContentReducerObj.deleteImage[imageContentFields[key]['id']]} key = {++i}>
-					<div className = "imgClass " key = {++i} id = {"imgId_" + imageContentFields[key]['id']}>
-						
-						<img src = {imageContentFields[key]['imageLocation']} width = "100%"/>
+				<div className={"col-md-3 col-sm-6 col-xs-12" + this.props.customContentReducerObj.deleteImage[imageContentFields[key]['id']]} key={++i}>
+					<div className="imgClass " key={++i} id={"imgId_" + imageContentFields[key]['id']}>
+
+						<img src={imageContentFields[key]['imageLocation']} width="100%" />
 						<div className="overlay">
 							<OpenImage
-								id = {imageContentFields[key]['id']}
-								handleOpenClick = {this.handleOpenClick}
-								imageModalVisibility = {this.props.customContentReducerObj.imageModalVisibility}
-								onImageCloseClick = {this.props.actions.closeImageModal.bind(this)}
-								showImage = {this.props.customContentReducerObj.clickedImage}
+								id={imageContentFields[key]['id']}
+								handleOpenClick={this.handleOpenClick}
+								imageModalVisibility={this.props.customContentReducerObj.imageModalVisibility}
+								onImageCloseClick={this.props.actions.closeImageModal.bind(this)}
+								showImage={this.props.customContentReducerObj.clickedImage}
 							/>
 
-				            <a  id = {"deleteImage_" + imageContentFields[key]['id'] } onClick = {this.deleteImage} className="expand">Delete</a>
-				            
-				            <a id = {"printImage_" + imageContentFields[key]['id'] } onClick = {this.printImage} className="expand">Print</a>
-				            
-				            <a className="closeOverlay hidden">x</a>
-				        </div>
+							<a id={"deleteImage_" + imageContentFields[key]['id']} onClick={this.deleteImage} className="expand">Delete</a>
+
+							<a id={"printImage_" + imageContentFields[key]['id']} onClick={this.printImage} className="expand">Print</a>
+
+							<a className="closeOverlay hidden">x</a>
+						</div>
 					</div>
-					<div className = "imageTextStyle">
+					<div className="imageTextStyle">
 						{imageContentFields[key]['imageText']}
 					</div>
 				</div>
 			);
 		}
 
-		return(
+		return (
 
-			<div className = "bottomSlideEffect effects clearfix">
+			<div className="bottomSlideEffect effects clearfix">
 				{imageContentHtml}
 			</div>
 		);
--- src/components/OpenImage.js
+++ src/components/OpenImage.js
@@ -2,29 +2,29 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Modal from 'react-modal';
 
-export default class OpenImage extends Component{
-	constructor(props){
-		super(props);
-	}
+export default class OpenImage extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-	render(){
-		var openId = this.props.id;
-		return(
-			<span>
-				<a className="expand" onClick = {this.props.handleOpenClick} id = {this.props.id} >Open</a>
-				<Modal isOpen={this.props.imageModalVisibility[openId]} contentLabel="Modal Window" className="ModalClass" overlayClassName="OverlayClass" >
+  render() {
+    var openId = this.props.id;
+    return (
+      <span>
+        <a className="expand" onClick={this.props.handleOpenClick} id={this.props.id} >Open</a>
+        <Modal isOpen={this.props.imageModalVisibility[openId]} contentLabel="Modal Window" className="ModalClass" overlayClassName="OverlayClass" >
 
-				<div className = 'centerContainer'>
-					<div className = 'mainContainer'>
-						<img src = {this.props.showImage[openId]} height = "400px" width = "100%"/>
+          <div className='centerContainer'>
+            <div className='mainContainer'>
+              <img src={this.props.showImage[openId]} height="400px" width="100%" />
 
-						<div className = "topPadding">
-		                    <button className = "btn btn-primary" onClick={this.props.onImageCloseClick}>Close</button>
-						</div>
-					</div>
-				</div>
-				</Modal>
-			</span>
-		);
-	}
+              <div className="topPadding">
+                <button className="btn btn-primary" onClick={this.props.onImageCloseClick}>Close</button>
+              </div>
+            </div>
+          </div>
+        </Modal>
+      </span>
+    );
+  }
 }
