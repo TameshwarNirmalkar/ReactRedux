337054f34b09690fa49a06f9dd61acea8ef33525
Tameshwar Nirmalkar <tameshwar.nirmalkar@scrumstart.net>
2019-05-28 13:11:13 +0530

    run from local

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import { BrowserRouter as Router, Route, Link, browserHistory, IndexRoute  } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, browserHistory, IndexRoute } from 'react-router-dom';
 
 import CustomContent from './components/CustomContent';
 import rootReducer from './reducers';
@@ -11,25 +11,25 @@ import rootReducer from './reducers';
 import './assets/css/main.scss';
 
 const store = createStore(rootReducer, compose(
-    applyMiddleware(thunk),
-    // window.devToolsExtension ? window.devToolsExtension() : f => f
+  applyMiddleware(thunk),
+  // window.devToolsExtension ? window.devToolsExtension() : f => f
 ));
 
 const attachElement = document.getElementById('customContentApp');
 
 class Home extends Component {
-    render(){
-        return (
-            <Provider store={store}>
-        	    <CustomContent />   
-            </Provider>
-        )
-    }
+  render() {
+    return (
+      <Provider store={store}>
+        <CustomContent />
+      </Provider>
+    )
+  }
 }
 
 render(
-        <Router>
-            <Route path="/" component={Home}/>
-        </Router>,
-    	attachElement
+  <Router>
+    <Route path="/" component={Home} />
+  </Router>,
+  attachElement
 );
